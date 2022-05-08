bfd0ee8d31f62bedeb6deb7b2769e0bf85e7c96c
Yassine-Chraa <yassine.chraa1@email.com>
2022-05-08 06:27:03 +0100

    ok

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,30 @@ export default {
       var board = null;
       const game = new Chess();
 
+      const whiteSquareGrey = "#a9a9a9";
+      const blackSquareGrey = "#696969";
+
+      function removeGreySquares() {
+        let square = document.querySelectorAll("#myBoard .square-55d63");
+        for (let i = 0; i < square.length; i++) {
+          square[i].style.backgroundColor = "";
+        }
+      }
+      function greySquare(square) {
+        let $square = document.querySelector("#myBoard .square-" + square);
+
+        let background = whiteSquareGrey;
+
+        if (
+          (" " + $square.classList[1] + " ")
+            .replace(/[\n\t]/g, " ")
+            .indexOf(" black-3c85d ") > -1
+        ) {
+          background = blackSquareGrey;
+        }
+
+        $square.style.backgroundColor = background;
+      }
       function onDragStart(source, piece) {
         // do not pick up pieces if the game is over
         if (game.game_over()) return false;
@@ -31,8 +55,9 @@ export default {
         }
       }
       function onDrop(source, target) {
+        removeGreySquares();
         // see if the move is legal
-        var move = game.move({
+        let move = game.move({
           from: source,
           to: target,
           promotion: "q", // NOTE: always promote to a queen for example simplicity
@@ -41,14 +66,38 @@ export default {
         // illegal move
         if (move === null) return "snapback";
       }
+      function onMouseoverSquare(square) {
+        // get list of possible moves for this square
+        let moves = game.moves({
+          square: square,
+          verbose: true,
+        });
+
+        // exit if there are no moves available for this square
+        if (moves.length === 0) return;
+
+        // highlight the square they moused over
+        greySquare(square);
+
+        // highlight the possible squares for this piece
+        for (var i = 0; i < moves.length; i++) {
+          greySquare(moves[i].to);
+        }
+      }
+      function onMouseoutSquare() {
+        removeGreySquares();
+      }
       function onSnapEnd() {
         board.position(game.fen());
       }
-      var config = {
+
+      let config = {
         draggable: true,
         position: "start",
         onDragStart: onDragStart,
         onDrop: onDrop,
+        onMouseoutSquare: onMouseoutSquare,
+        onMouseoverSquare: onMouseoverSquare,
         onSnapEnd: onSnapEnd,
       };
 
